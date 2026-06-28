8e55cb45097e5a6a76755cc5630c8faa18b1bd52
Sourav H M <souravhm@Souravs-MacBook-Air.local>
2026-06-28 23:02:08 +0530

    MIND SQUARE

--- theme-init.js
+++
@@ -1,9 +0,0 @@
-// Apply theme immediately to prevent flashing
-(function () {
-    const savedTheme = localStorage.getItem('theme') || 'dark';
-    if (savedTheme === 'light') {
-        document.documentElement.classList.remove('dark');
-    } else {
-        document.documentElement.classList.add('dark');
-    }
-})();
